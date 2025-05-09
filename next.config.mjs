8135807699fe58b7ad9d78ab6bbc239e3fbd5c61
Alp Başar <138521262+AlpBasar@users.noreply.github.com>
2025-05-09 23:14:58 +0300

    pages güncellendi

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // GitHub Pages için statik dışa aktarmayı etkinleştirir
+
+   basePath: '/excel-harita',
+   assetPrefix: '/excel-harita/',
+
 };
 
 export default nextConfig;
